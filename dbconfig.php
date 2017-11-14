87a25781cba15d5944a9ba24abc8b6b0c60e4621
jesign <jixzignacio@gmail.com>
2017-11-14 16:49:34 +0800

    deleted dbconfig

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	header('Content-Type: application/json');	
-	$servername = "localhost";
-	$dbusername = "root";
-	$dbpassword = "jesign";
-	$dbname = "tiktak";
-
-	$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
