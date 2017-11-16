4513f44a2dc4eda4f6984f357bb098a8342bc9c0
jesign <jixzignacio@gmail.com>
2017-11-16 19:36:18 +0800

    insert vendor updates

--- vendor/insert.php
+++ vendor/insert.php
@@ -1,42 +1,45 @@
 <?php 
 include('../dbconfig.php');
 
-if (isset($_POST['fname']) && 
-	isset($_POST['lname']) && 
-	isset($_POST['email']) && 
-	isset($_POST['password'])
+	if (isset($_POST['fname']) && 
+		isset($_POST['lname']) && 
+		isset($_POST['email']) && 
+		isset($_POST['password'])
+	)
 	{
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		
+		
+		if ($conn->connect_error) {
+			// die("Connection failed: " . $conn->connect_error);
+			echo json_encode([
+				'status' => 'error', 
+				'message' => 'Connection Failed ' . $conn->connect_error
+			]);
+		}
 
+		$sql = "INSERT INTO vendors (fname, lname, email, password) VALUES ('$fname', '$lname', '$email', '$password')";
 
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	
+		if ($conn->query($sql) === TRUE) {
+		    echo json_encode([
+		    	'status' => 'success', 
+		    	'message' => 'Record inserted successfully'
+		    ]);
+		} else {
+		    echo json_encode([
+		    	'status' => 'error',
+		    	'message' =>  $conn->error
+		    ]);
+		}
 
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
+		$conn->close();
+	}else{
+		echo json_encode([
+			'status' => 'error', 
+			'message' => 'Please include all required fields'
+		]);
 	}
-
-	$sql = "INSERT INTO vendors (fname, lname, email, password) VALUES ('$fname', '$lname', '$email', '$password')";
-
-	if ($conn->query($sql) === TRUE) {
-	    echo json_encode([
-	    	'status' => 'success', 
-	    	'message' => 'Record inserted successfully'
-	    ]);
-	} else {
-	    echo json_encode([
-	    	'status' => 'error',
-	    	'message' =>  $conn->error
-	    ]);
-	}
-
-	$conn->close();
-}else{
-	echo json_encode([
-		'status' => 'error', 
-		'message' => 'Please include all required fields'
-	]);
-}
 ?>
