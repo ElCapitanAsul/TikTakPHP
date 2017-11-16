a0a91bbdac0391777afd1c7a9ffd8f5bcc8f7fb7
ElCapitanAsul <cmanero@uic.edu.ph>
2017-11-16 14:51:05 +0800

    updated fields for vendor signup

--- vendor/insert.php
+++ vendor/insert.php
@@ -5,28 +5,20 @@ if (isset($_POST['fname']) &&
 	isset($_POST['lname']) && 
 	isset($_POST['email']) && 
 	isset($_POST['password']) && 
-	isset($_POST['rating']) && 
-	isset($_POST['comment']) && 
-	isset($_POST['latitude']) && 
-	isset($_POST['longitude']) && 
-	isset($_POST['image'])) {
+	{
 
 
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
-	$rating = $_POST['rating'];
-	$comment = $_POST['comment'];
-	$latitude = $_POST['latitude'];
-	$longitude = $_POST['longitude'];
-	$image = $_POST['image'];
+	
 
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
 
-	$sql = "INSERT INTO vendors (fname, lname, email, password, rating, comment, latitude, longitude, image) VALUES ('$fname', '$lname', '$email', '$password', '$rating', '$comment', '$latitude', '$longitude', '$image')";
+	$sql = "INSERT INTO vendors (fname, lname, email, password) VALUES ('$fname', '$lname', '$email', '$password')";
 
 	if ($conn->query($sql) === TRUE) {
 	    echo json_encode([
